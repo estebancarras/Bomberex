91e2d424994a000a7a5dc62998ee1abff344d86e
EstebanCarrasco <est.carrasco@duocuc.cl>
2025-05-26 18:46:56 -0400

    mejora mantenimiento v1
    estoy mejorando el apartado de mantenimiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,20 @@ export const routes: Routes = [
   { path: 'home', loadComponent: () => /* @vite-ignore */ import('./home/home.page').then(m => m.HomePage) },
   { path: 'vehiculos', loadComponent: () => /* @vite-ignore */ import('./vehiculos/vehiculos.page').then(m => m.VehiculosPage) },
   { path: 'detalle-vehiculo/:id', loadComponent: () => /* @vite-ignore */ import('./detalle-vehiculo/detalle-vehiculo.page').then(m => m.DetalleVehiculoPage) },
-  { path: 'mantenimiento', loadComponent: () => /* @vite-ignore */ import('./mantenimiento/mantenimiento.page').then(m => m.MantenimientoPage) },
+  {
+    path: 'mantenimiento',
+    loadComponent: () => import('./mantenimiento-home/mantenimiento-home.page').then(m => m.MantenimientoHomePage),
+    children: [
+      {
+        path: 'add',
+        loadComponent: () => import('./mantenimiento/mantenimiento.page').then(m => m.MantenimientoPage)
+      },
+      {
+        path: 'list',
+        loadComponent: () => import('./mantenimientos-list/mantenimientos-list.page').then(m => m.MantenimientosListPage)
+      }
+    ]
+  },
   { path: 'perfil', loadComponent: () => /* @vite-ignore */ import('./perfil/perfil.page').then(m => m.PerfilPage) },
   {
     path: 'registro',
---
+++ src/app/mantenimientos-list/mantenimientos-list.page.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonListHeader } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-mantenimientos-list',
+  templateUrl: './mantenimientos-list.page.html',
+  styleUrls: ['./mantenimientos-list.page.scss'],
+  standalone: true,
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonListHeader]
+})
+export class MantenimientosListPage {
+  private firestore = inject(Firestore);
+  mantenimientos$: Observable<any[]> = collectionData(collection(this.firestore, 'mantenimientos'), { idField: 'id' });
+}
